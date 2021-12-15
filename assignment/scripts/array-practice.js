7869e76212c57fc6f7933881a7f9466ce135f9ad
Heather Kim <heatherkim0405@gmail.com>
2021-12-15 10:14:48 -0600

    assignment1 up until 3.c

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -22,7 +22,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-
+console.log('number of foods: ', favFoods.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -31,14 +31,14 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array
-
+console.log('The second animal is ', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
-
+console.log('The last animal is ', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-
+console.log('The last animal is ', animalArray[animalArray.length - 1]);
 
 
 // 4. Adding & Removing Array Items
