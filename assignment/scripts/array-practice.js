f8c4a7e1bc5441d1128cd005d745617ca9e07825
Heather Kim <heatherkim0405@gmail.com>
2021-12-16 17:19:10 -0600

    final version

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -12,7 +12,7 @@ let favFoods = ['apples', 'Chocolate', 'Gimbap', 'pizza'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar
 //      to the example above
-console.log('My favorite foods are ', favFoods);
+console.log('My favorite foods are', favFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -22,7 +22,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-console.log('number of foods: ', favFoods.length);
+console.log('number of foods:', favFoods.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -31,10 +31,10 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array
-console.log('The second animal is ', animalArray[1]);
+console.log('The second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
-console.log('The last animal is ', animalArray[3]);
+console.log('The last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
@@ -61,8 +61,8 @@ console.log('The animals are now', animalArray);
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
 let removedFood = favFoods.pop();
-console.log('Removed the last food ', removedFood);
-console.log('The favorite foods are now ', favFoods);
+console.log('Removed the last food', removedFood);
+console.log('The favorite foods are now', favFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
